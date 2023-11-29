06457f697b92c3a946b341f369a3ff06ddc2fd3a
ryan-friberg <ref2156@columbia.edu>
2023-11-29 13:23:03 -0500

    updating dataset files

--- data/dataset.py
+++ data/dataset.py
@@ -1,11 +1,11 @@
 import datasets
-import glob
-import itertools
-import numpy as np
+from datasets import load_dataset
 import os
 from PIL import Image
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import subprocess
 
 
 '''
@@ -23,6 +23,21 @@ we move each non HF dataset to the same directory as well.
 
 For now, all images are saved as 32x32 RGB to match that of the huggingface
 datasets.
+
+Dependencies: 
+- pip install datasets
+- pip install kaggle
+
+This file's purpose is to acquire the necessary data if it does not
+yet exist on the filesystem.
+
+The expected disk format (for the data-preprocessing) is each respective
+platform's default representation.
+
+Datasets used:
+https://huggingface.co/datasets/detectors/lsun_c-ood
+https://huggingface.co/datasets/detectors/lsun_r-ood
+https://www.kaggle.com/datasets/ashwingupta3012/human-faces/
 '''
 
 
@@ -34,7 +49,11 @@ class CBRDataSet(Dataset):
         
         # mainly for if we scrape the web
         self.valid_file_types = ['.jpg', '.jpeg', '.png']
-        
+        self.download()
+
+        if (not os.path.exists(self.unified_dir)):
+            os.mkdir(self.unified_dir)
+
         if (len(os.listdir(self.unified_dir)) == 0):
             print("Pre-processing data!")
             self.preprocess_datasets()
@@ -51,12 +70,36 @@ class CBRDataSet(Dataset):
     def __len__(self):
         return self.num_images 
 
+    def download(self):
+        lsun1_path = "./datasets/lsun1"
+        lsun2_path = "./datasets/lsun2"
+        faces_path = "./datasets/faces"
+
+        if ((not os.path.exists(lsun1_path)) or (len(os.listdir(lsun1_path)) == 0)):
+            lsun1_data = load_dataset("detectors/lsun_c-ood")
+            lsun1_data.save_to_disk(lsun1_path)
+        else:
+            print("Skipping lsun1 download")
+        
+        if ((not os.path.exists(lsun2_path)) or (len(os.listdir(lsun2_path)) == 0)):
+            lsun2_data = load_dataset("detectors/lsun_r-ood")
+            lsun2_data.save_to_disk(lsun2_path)
+        else:
+            print("Skipping lsun2 download")
+
+        if ((not os.path.exists(faces_path)) or (len(os.listdir(faces_path)) == 0)):
+            os.mkdir(faces_path)
+            download_cmd = 'kaggle datasets download -d ashwingupta3012/human-faces -p %s' % faces_path
+            unzip_cmd = 'unzip %s/human-faces.zip -d %s' % (faces_path, faces_path)
+            result = subprocess.run(download_cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+            result = subprocess.run(unzip_cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+        else:
+            print("Skipping face data download")
+        print("Datasets are up to date!")
+
     # extract, reshape, and combine the images across multiple datasets
     def preprocess_datasets(self):
         img_index = 0
-        if (not os.path.exists(self.unified_dir)):
-            os.mkdir(self.unified_dir)
-
         for (dir, type) in self.data_dir_list:
             if (type == 'hf'):
                 dataset = datasets.load_from_disk(dir)
@@ -65,9 +108,10 @@ class CBRDataSet(Dataset):
                     img_name = "%d.jpg" % img_index
                     img_file = os.path.join(self.unified_dir, img_name)
                     img = img_dict['image']
-                
+
                     # reshape our image/convert to RGB and save
                     img = img.convert('RGB')
+                    img = img.resize((36,36))
                     img.save(img_file)
                     img_index += 1
             elif (type == 'kaggle'):
@@ -82,7 +126,7 @@ class CBRDataSet(Dataset):
                     
                     # reshape our image/convert to RGB and save
                     img = img.convert('RGB')
-                    img = img.resize((32,32))
+                    img = img.resize((36,36))
                     img.save(new_img_file)
                     img_index += 1
 
@@ -109,9 +153,12 @@ def collate_fn(batch):
 
 
 # example build
-# if __name__=='__main__':
-#     ls = [('./datasets/lsun1/train', 'hf'),
-#           ('./datasets/lsun2/train', 'hf'),
-#           ('./datasets/faces/Humans', 'kaggle')]
-#     dir = './datasets/data'
-#     test = CBRDataSet(ls, dir, None)
+if __name__=='__main__':
+    ls = [('./datasets/lsun1/train', 'hf'),
+          ('./datasets/lsun2/train', 'hf'),
+          ('./datasets/faces/Humans', 'kaggle')]
+    dir = './datasets/data'
+    test = CBRDataSet(ls, dir, transforms=transforms.ToTensor())
+    dataloader = DataLoader(test, batch_size=4)
+    for x in iter(dataloader):
+        print(x.shape)

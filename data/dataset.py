12675fbce855900841c4fed5d5a16e40f1508c3a
ryan-friberg <ref2156@columbia.edu>
2023-12-01 18:54:42 -0500

    switched dataset source and fixed the transformer

--- data/dataset.py
+++ data/dataset.py
@@ -1,146 +1,93 @@
-import datasets
-from datasets import load_dataset
+from astroNN.datasets import load_galaxy10  # this may throw a tensorflow error
+import glob
+import h5py
+import itertools
+import numpy as np
 import os
 from PIL import Image
 import torch
+
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
-import subprocess
 
 
 '''
-This file defines our custom pytorch Dataset class that conglomerates
-and accesses the files downloaded across many datasets. As conten-based
-retrieval can be structured as an unsupervised or self-supervised task,
-we do not need ot create a mapping between images and their respective
-ground truth labels.
-
-Current conglomeration approach:
-Huggingface stores their datasets on the filesystem as a .hf face, so for
-each HF dataset given, we extract and resave each datapoint as its own file
-to avoid needing to load the dataset again on __getitem__. For simplicitly,
-we move each non HF dataset to the same directory as well.
-
-For now, all images are saved as 32x32 RGB to match that of the huggingface
-datasets.
-
-Dependencies: 
-- pip install datasets
-- pip install kaggle
-
-This file's purpose is to acquire the necessary data if it does not
-yet exist on the filesystem.
-
-The expected disk format (for the data-preprocessing) is each respective
-platform's default representation.
+This file defines our custom pytorch Dataset class using the
+Galaxy10 dataset.
 
 Datasets used:
-https://huggingface.co/datasets/detectors/lsun_c-ood
-https://huggingface.co/datasets/detectors/lsun_r-ood
-https://www.kaggle.com/datasets/ashwingupta3012/human-faces/
+https://astronn.readthedocs.io/en/latest/galaxy10.html
 '''
 
 
-class CBRDataSet(Dataset):
-    def __init__(self, data_dir_list, unified_dir, transforms):
-        self.data_dir_list = data_dir_list
-        self.unified_dir = unified_dir
+class GalaxyCBRDataSet(Dataset):
+    def __init__(self, images_dir, transforms, force_download=False, h5_file=None):
         self.transforms = transforms
-        
-        # mainly for if we scrape the web
-        self.valid_file_types = ['.jpg', '.jpeg', '.png']
-        self.download()
+        self.supported_file_types = ['/*.jpg']
+        self.images_dir = images_dir 
 
-        if (not os.path.exists(self.unified_dir)):
-            os.mkdir(self.unified_dir)
+        if (not os.path.exists(images_dir)):
+            os.mkdir(images_dir)
 
-        if (len(os.listdir(self.unified_dir)) == 0):
-            print("Pre-processing data!")
-            self.preprocess_datasets()
+        if force_download:
+            self.download_galaxy10_data(h5_file)
         
-        self.image_files = self.get_images()
+        image_files, labels = self.get_image_filenames_with_labels(self.images_dir)
+        self.image_files = np.array(image_files)
+        self.labels = np.array(labels).astype("int")
         self.num_images = len(self.image_files)
 
     def __getitem__(self, idx):
-        img = Image.open(self.image_files[idx])
-        if self.transforms:
-            img = self.transforms(img)
-        return img
+        try:
+            image = Image.open(self.image_files[idx]).convert('RGB')
+            if image.size != (224,224):
+                image = image.resize((224,224))
+            label = self.labels[idx]
+            image = self.transforms(image)
+            return image, label
+        except:
+            return None
 
     def __len__(self):
         return self.num_images 
 
-    def download(self):
-        lsun1_path = "./datasets/lsun1"
-        lsun2_path = "./datasets/lsun2"
-        faces_path = "./datasets/faces"
-
-        if ((not os.path.exists(lsun1_path)) or (len(os.listdir(lsun1_path)) == 0)):
-            lsun1_data = load_dataset("detectors/lsun_c-ood")
-            lsun1_data.save_to_disk(lsun1_path)
+    def download_galaxy10_data(self, h5_filename=None):
+        if h5_filename:
+            with h5py.File(h5_filename, 'r') as F:
+                images = np.array(F['images'])
+                labels = np.array(F['ans'])
         else:
-            print("Skipping lsun1 download")
-        
-        if ((not os.path.exists(lsun2_path)) or (len(os.listdir(lsun2_path)) == 0)):
-            lsun2_data = load_dataset("detectors/lsun_r-ood")
-            lsun2_data.save_to_disk(lsun2_path)
-        else:
-            print("Skipping lsun2 download")
+            images, labels = load_galaxy10()
 
-        if ((not os.path.exists(faces_path)) or (len(os.listdir(faces_path)) == 0)):
-            os.mkdir(faces_path)
-            download_cmd = 'kaggle datasets download -d ashwingupta3012/human-faces -p %s' % faces_path
-            unzip_cmd = 'unzip %s/human-faces.zip -d %s' % (faces_path, faces_path)
-            result = subprocess.run(download_cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True)
-            result = subprocess.run(unzip_cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True)
-        else:
-            print("Skipping face data download")
-        print("Datasets are up to date!")
+        label_set = np.unique(labels)
+        for unique_label in label_set:
+            label_dir = os.path.join(self.images_dir, str(int(unique_label)))
+            if (not os.path.exists(label_dir)):
+                os.mkdir(label_dir)
 
-    # extract, reshape, and combine the images across multiple datasets
-    def preprocess_datasets(self):
-        img_index = 0
-        for (dir, type) in self.data_dir_list:
-            if (type == 'hf'):
-                dataset = datasets.load_from_disk(dir)
-                for img_dict in dataset:
-                    # extract file name information and build new filename
-                    img_name = "%d.jpg" % img_index
-                    img_file = os.path.join(self.unified_dir, img_name)
-                    img = img_dict['image']
+            indices = np.where(labels == unique_label)[0]
+            image_idx = 0
+            print("Saving class", str(unique_label), "images...")
+            for idx in indices:
+                file_name = os.path.join(label_dir, str(image_idx) + ".jpg")
+                image = Image.fromarray(images[idx].astype('uint8'))
+                image.save(file_name)
+                image_idx += 1
 
-                    # reshape our image/convert to RGB and save
-                    img = img.convert('RGB')
-                    img = img.resize((36,36))
-                    img.save(img_file)
-                    img_index += 1
-            elif (type == 'kaggle'):
-                imgs = os.listdir(dir)
-                for img_file in imgs:
-                    # extract file name information and build new filename
-                    file, type = os.path.splitext(img_file)
-                    name = os.path.join(dir, img_file)
-                    img = Image.open(name)
-                    new_img = str(img_index) + type
-                    new_img_file = os.path.join(self.unified_dir, new_img)
-                    
-                    # reshape our image/convert to RGB and save
-                    img = img.convert('RGB')
-                    img = img.resize((36,36))
-                    img.save(new_img_file)
-                    img_index += 1
+    def get_image_filenames_with_labels(self, images_dir):
+        image_files = []
+        labels = []
 
-    # walk through each file names in the pre-processed dir and collect valid files
-    def get_images(self):
-        images = []
-        files = os.listdir(self.unified_dir)
-        for img_file in files:
-            file, type = os.path.splitext(img_file)
-            if (img_file == ".DS_Store") or (type not in self.valid_file_types):
+        files = os.listdir(images_dir)
+        for name in files:
+            if name == ".DS_Store":
                 continue
-            file_name = os.path.join(self.unified_dir, img_file)
-            images.append(file_name)
-        return images
+            image_class_dir = os.path.join(images_dir, name)
+            image_class_files = list(itertools.chain.from_iterable(
+                [glob.glob(image_class_dir + file_type) for file_type in self.supported_file_types]))
+            image_files += image_class_files
+            labels += [int(name)] * len(image_class_files)
+        return image_files, labels
 
 
 # simple collation function to be used in the future for the DataLoader
@@ -154,11 +101,9 @@ def collate_fn(batch):
 
 # example build
 if __name__=='__main__':
-    ls = [('./datasets/lsun1/train', 'hf'),
-          ('./datasets/lsun2/train', 'hf'),
-          ('./datasets/faces/Humans', 'kaggle')]
-    dir = './datasets/data'
-    test = CBRDataSet(ls, dir, transforms=transforms.ToTensor())
+    dir = './galaxy_dataset/'
+    test = GalaxyCBRDataSet(dir, transforms=transforms.ToTensor(), force_download=False)
     dataloader = DataLoader(test, batch_size=4)
-    for x in iter(dataloader):
-        print(x.shape)
+    for batch, labels in iter(dataloader):
+        print(batch.shape, labels.shape)
+        # print(x.shape)
--- models/transformer_feature_extractor.py
+++ models/transformer_feature_extractor.py
@@ -9,14 +9,16 @@ This file's purpose is to define the learning-based models used for feature extr
 architectures include a vision transformer, and a CNN-based variational autoencoder.
 
 This file may potentially include pre-trained and from-scratch models.
+
+Implementation architecture from: https://arxiv.org/pdf/2010.11929.pdf 
 '''
 
 # backward pass is handled by the autograd functionality of pytorch
 class FeatureExtractorViT(nn.Module):
     # n_patches is number of patches along a dimension
-    def __init__(self, batch_shape, n_patches=6, hidden_size=128, num_blocks=1, num_heads=4, output_feature_size=256):
+    def __init__(self, batch_shape, n_patches=14, hidden_size=128, num_blocks=1, num_heads=4, output_feature_size=256):
         super(FeatureExtractorViT, self).__init__()
-        assert (batch_shape[1:] == (3,36,36)) # the econding works only for specific image shape
+        assert (batch_shape[1:] == (3,224,224)) # the econding works only for specific image shape
         
         ### assign class variables
         self.shape = batch_shape
@@ -31,16 +33,16 @@ class FeatureExtractorViT(nn.Module):
 
         ### define the actual transformer model architecture
         self.linear         = nn.Linear(int(batch_shape[0] * self.p_shape[0] * self.p_shape[1]), hidden_size)
-        attn_layers         = [EncoderBlock(hidden_size, num_heads) for _ in range(num_blocks)]
+        attn_layers         = [ViTEncoderBlock(hidden_size, num_heads) for _ in range(num_blocks)]
         self.encoder_blocks = nn.Sequential(*attn_layers)
         self.output_layer   = nn.Linear(hidden_size, output_feature_size)
         
         ### define positional embedding, mark it untrainable, and add it to the state_dict
-        self.pos_embed = nn.Parameter(self.pos_embedding(n_patches ** 2, hidden_size))
+        self.pos_embed = nn.Parameter(self.positional_embedding(n_patches ** 2, hidden_size))
         self.pos_embed.requires_grad = False
-        self.register_buffer('positional_embedding', self.pos_embed, persistent=False)
+        self.register_buffer('pos_embedding', self.pos_embed, persistent=False)
 
-    def pos_embedding(self, sequence_len, embed_dim):
+    def positional_embedding(self, sequence_len, embed_dim):
         position = torch.arange(0, sequence_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, embed_dim, 2).float() * -(np.log(10000.0) / embed_dim))
         embeddings = torch.zeros(sequence_len, embed_dim)
@@ -49,56 +51,67 @@ class FeatureExtractorViT(nn.Module):
         return embeddings
 
     def forward(self, batch):
-        img_patches = batch.unfold(2, self.p_size, self.p_size).unfold(3, self.p_size, self.p_size)
-        img_patches = img_patches.reshape(batch.shape[0], batch.shape[1], -1, self.p_size * self.p_size)
-        z = self.linear(img_patches)
-        pos_embed = self.pos_embed.repeat(batch.shape[0], batch.shape[1], 1, 1)
+        # expected shape: batch_size x num_channels x height x width
+        # treat each channel as unique image within the batch
+        batch = batch.view(-1, 1, batch.size(2), batch.size(3))
+
+        # unfold the images in the batch into tensors
+        # first reshape the patches to have shape (batch_size, num_patches, channels, patch_size, patch_size)
+        # then reshape to (batch_size*3, num_patches_per_image, num_elements_in_patch)
+        patches = batch.unfold(2, self.p_size, self.p_size).unfold(3, self.p_size, self.p_size)
+        patches = patches.permute(0, 2, 3, 1, 4, 5).contiguous().view(batch.size(0), -1, batch.size(1), self.p_size, self.p_size)
+        patches = patches.view(batch.size(0), -1, self.p_size * self.p_size)
+
+        z = self.linear(patches)
+        pos_embed = self.pos_embed.repeat(batch.shape[0], 1, 1)
         z += pos_embed
         z = self.encoder_blocks(z)
         z = self.output_layer(z)
         return z
-    
 
-class AttentionEncoder(nn.Module):
+
+class MultiHeadAttentionEncoder(nn.Module):
     def __init__(self, dim, num_heads=3):
-        super(AttentionEncoder, self).__init__()
+        super(MultiHeadAttentionEncoder, self).__init__()
         self.dim = dim
         self.num_heads = num_heads
-
-        # query, key, value mappings
         self.head_dim = int(dim // num_heads)
-        self.q_map = nn.ModuleList([nn.Linear(self.head_dim, self.head_dim) for q in range(self.num_heads)])
-        self.k_map = nn.ModuleList([nn.Linear(self.head_dim, self.head_dim) for k in range(self.num_heads)])
-        self.v_map = nn.ModuleList([nn.Linear(self.head_dim, self.head_dim) for v in range(self.num_heads)])
+        print("mha", dim, num_heads, self.head_dim)
+
+        # create the query, key, value layers (each list is the same length the entries at index i represent attn head i)
+        self.multihead_q = nn.ModuleList([nn.Linear(self.head_dim, self.head_dim) for q in range(self.num_heads)])
+        self.multihead_k = nn.ModuleList([nn.Linear(self.head_dim, self.head_dim) for k in range(self.num_heads)])
+        self.multihead_v = nn.ModuleList([nn.Linear(self.head_dim, self.head_dim) for v in range(self.num_heads)])
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, linear_embed):
+        # linear embedding has shape of 
         output = []
-        for sequence in linear_embed:
-            seq_z = []
-            for seq_channel in sequence:
-                channel_z = []
-                for head in range(self.num_heads):
-                    # seq = seq_channel[:, :, head * self.head_dim: (head + 1) * self.head_dim].squeeze(0)
-                    seq = seq_channel[:, head * self.head_dim: (head + 1) * self.head_dim]
-                    q = self.q_map[head](seq)
-                    k = self.k_map[head](seq) 
-                    v = self.v_map[head](seq)
-                    attention = self.softmax(q @ k.T / np.sqrt(self.head_dim))
-                    channel_z.append(attention @ v)
-                seq_z.append(torch.hstack(channel_z))
-            output.append(torch.hstack(seq_z))
-        print(torch.cat([torch.unsqueeze(z, dim=0) for z in output]).shape)
+        for img in linear_embed:
+            # img is of shape num_patches x patch_len
+            attn_res = []
+            for head in range(self.num_heads):
+                head_input = img[:, head * self.head_dim: (head + 1) * self.head_dim]
+                q = self.multihead_q[head](head_input)
+                k = self.multihead_k[head](head_input) 
+                v = self.multihead_v[head](head_input)
+
+                # matmul k and q, (technically scale, then mask) then apply softmax
+                # matmul the total result with value tensor
+                attn = self.softmax(q @ k.T / np.sqrt(self.head_dim)) @ v
+                attn_res.append(attn)
+
+        output.append(torch.hstack(attn_res))
         return torch.cat([torch.unsqueeze(z, dim=0) for z in output])
 
 
-class EncoderBlock(nn.Module):
+class ViTEncoderBlock(nn.Module):
     def __init__(self, hidden_size, num_heads):
-        super(EncoderBlock, self).__init__()
+        super(ViTEncoderBlock, self).__init__()
         self.hidden_size = hidden_size
         self.num_heads = num_heads
         self.attention = nn.Sequential(nn.LayerNorm(self.hidden_size),
-                                       AttentionEncoder(self.hidden_size, self.num_heads))
+                                       MultiHeadAttentionEncoder(self.hidden_size, self.num_heads))
         self.layer_norm = nn.LayerNorm(self.hidden_size)
         self.mlp = nn.Sequential(nn.Linear(self.hidden_size, 2*self.hidden_size),
                                  nn.ReLU(),
@@ -112,11 +125,13 @@ class EncoderBlock(nn.Module):
 
 if __name__=='__main__':
     from PIL import Image 
-    test_file = "../data/datasets/data/0.jpg"
+    test_file = "../data/galaxy_dataset/1/0.jpg"
     img = Image.open(test_file)
     img = img.convert('RGB')
     x = torchvision.transforms.ToTensor()(img).unsqueeze(0)
+    x = torchvision.transforms.Resize(224)(x)
+
     # x = torchvision.transforms.Grayscale()(x).unsqueeze(0)
-    test = FeatureExtractorViT(batch_shape=(1,3,36,36))
+    test = FeatureExtractorViT(batch_shape=(1,3,224,224))
     output = test(x)
     print(output.shape)
